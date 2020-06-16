c7eca1a91535c74034d6d659568ddd6c48adc98c
kangituser <yehonatana@kan.org.il>
2020-06-16 14:52:55 +0300

    header fixes

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ app.use("/service-request", serviceRequestRoutes);
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Methods", `POST ,GET ,OPTIONS, PUT, PATCH, DELETE`);
   res.setHeader("Access-Control-Allow-Headers", `Content-Type , Authorization ,Cache-Control, multipart/form-data , application/json ,text/plain, text/html`);
   if (req.method == "OPTIONS") {
