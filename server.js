1d4bf829d35192f152023a0654283e718b7637f8
kangituser <yehonatana@kan.org.il>
2020-06-16 14:52:29 +0300

    removed headers

--- server.js
+++ server.js
@@ -20,9 +20,8 @@ app.use("/service-request", serviceRequestRoutes);
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Methods", `POST ,GET ,OPTIONS, PUT, PATCH, DELETE, ACL, CANCELUPLOAD, CHECKIN, CHECKOUT, COPY, HEAD, LOCK, MKCALENDAR, MKCOL, MOVE, PROPFIND, PROPPATCH, REPORT, SEARCH, UNCHECKOUT, UNLOCK, UPDATE, VERSION-CONTROL`);
-  res.setHeader("Access-Control-Allow-Headers", `Content-Type , Authorization ,Cache-Control, multipart/form-data , application/json ,text/plain, text/html, Overwrite, Destination, Depth, User-Agent, Translate, Range, Content-Range, Timeout, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, Location, Lock-Token, If`);
-  res.setHeader("Access-Control-Expose-Headers", "DAV, content-length, Allow")
+  res.setHeader("Access-Control-Allow-Methods", `POST ,GET ,OPTIONS, PUT, PATCH, DELETE`);
+  res.setHeader("Access-Control-Allow-Headers", `Content-Type , Authorization ,Cache-Control, multipart/form-data , application/json ,text/plain, text/html`);
   if (req.method == "OPTIONS") {
     return res.status(200);
   }
