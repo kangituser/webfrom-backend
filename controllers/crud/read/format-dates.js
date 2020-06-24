c89dc14f89272de940b7c47de9c1f391ac24d9da
kangituser <yehonatana@kan.org.il>
2020-06-24 13:47:38 +0300

    unix time

--- controllers/crud/read/format-dates.js
+++ controllers/crud/read/format-dates.js
@@ -1,3 +1,4 @@
-const formatDate = dateTime => dateTime ? new Date(dateTime).toLocaleString('he-IL',{ timezone: "Asia/Jerusalem" }) : null; 
+// const formatDate = dateTime => dateTime ? new Date(dateTime).toLocaleString('he-IL',{ timezone: "Asia/Jerusalem" }) : null; 
+const formatDate = dateTime => dateTime ? new Date(dateTime).getTime() : null; 
 
 module.exports = { formatDate };
